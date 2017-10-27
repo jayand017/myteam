bc82d8b92398450f0756495dcf55018fdfd97b4f
nimitbhargava <nimitbhargava@gmail.com>
2017-10-27 18:19:01 +0530

    refactoring-1

--- add_sale.php
+++ add_sale.php
@@ -11,7 +11,7 @@ $cust_phone = $_POST["cust_phone"];
 $cust_address = $_POST["cust_address"];
 $sale_amount = $_POST["sale_amount"];
 $card_no = $_POST["card_no"];
-$card_exp = $_POST["card_exp_mm"]."/".$_POST["card_exp_yy"];
+$card_exp = $_POST["card_exp_mm"] . "/" . $_POST["card_exp_yy"];
 $card_user = $_POST["card_user"];
 $tech_issue = $_POST["tech_issue"];
 $soft_plan = $_POST["soft_plan"];
@@ -23,12 +23,11 @@ $agent_id = $_SESSION["agent_id"];
 //Create a object
 $db = new DB();
 $ia = new InsertSale();
-$bol = $ia -> insert_sale($db, $sale_date, $cust_name, $cust_email, $cust_phone, $cust_address, $sale_amount, $card_no, $card_exp, $card_user, $tech_issue, $soft_plan, $tech_plan, $remark, $agent_id);
+$bol = $ia->insert_sale($db, $sale_date, $cust_name, $cust_email, $cust_phone, $cust_address, $sale_amount, $card_no, $card_exp, $card_user, $tech_issue, $soft_plan, $tech_plan, $remark, $agent_id);
 
-if($bol === true) {
-    exit(header("Location:sales.php?err=1"));      
-}
-else {
+if ($bol === true) {
+    exit(header("Location:sales.php?err=1"));
+} else {
     exit(header("Location:sales.php?err=-1"));
 }
 
--- conf/config.php
+++ conf/config.php
@@ -1,67 +1,68 @@
 <?php
 
-class DB {
+class DB
+{
     //database connection variables
     protected static $myconn;
-    
+
     //Specify the database credentials here
     protected $db_host = 'localhost';
     protected $db_user = 'root';
     protected $db_pass = '';
     protected $db_name = 'myteam';
-    
-    
+
+
     /**
      * Connect to the database!
      */
-    public function connect() {
+    public function connect()
+    {
         //Connection String
-        $conn= new mysqli($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
+        $conn = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
 
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
-        }
-        else {
+        } else {
             $this->myconn = $conn;
             //echo "Connection established";
         }
         return $this->myconn;
-          
+
     }
-    
+
     /**
      * Query the database
      */
-    public function query($query) {
+    public function query($query)
+    {
         // Connect to the database
-        $myconn = $this -> connect();
+        $myconn = $this->connect();
 
         // Query the database
-        $result = $myconn -> query($query);
+        $result = $myconn->query($query);
 
         return $result;
     }
-    
+
     /**
      * Fetch rows from the database (SELECT query)
      *
      */
-    public function select($query) {
+    public function select($query)
+    {
         $rows = array();
-        $result = $this -> query($query);
-        if($result === false) {
+        $result = $this->query($query);
+        if ($result === false) {
             return false;
         }
-        while ($row = $result -> fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
             $rows[] = $row;
         }
         return $rows;
     }
-    
-    
+
+
 }//End class
 
 
-
-    
 ?>
